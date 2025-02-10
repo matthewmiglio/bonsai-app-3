a2ed21b45df1af588d63146cd380610b437bf553
Matt Main_PC <matmigg0804@gmail.com>
2025-02-10 11:26:12 -0500

    typing for requests

--- src/pages/api/signUpTable.ts
+++ src/pages/api/signUpTable.ts
@@ -1,17 +1,21 @@
+import { NextApiRequest, NextApiResponse } from "next";
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.SUPA_URL || "no supa table url in env";
 const supabaseKey = process.env.SUPA_SECRET_KEY || "no supa key in env";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-export default async function handler(req: any, res: any) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method === "POST") {
     const { fname, lname, email, phone } = req.body;
 
     try {
-      console.log('Received a request to add a user with the following data:', req.body);
+      console.log("Received a request to add a user with the following data:", req.body);
       const { data, error } = await supabase.from("SIGNUPs").insert([
-        { fname: fname, lname: lname, email: email, phone:phone },
+        { fname: fname, lname: lname, email: email, phone: phone },
       ]);
 
       if (error) {
@@ -22,11 +26,11 @@ export default async function handler(req: any, res: any) {
 
       return res.status(200).json({ message: "User successfully added", data });
     } catch (error) {
-      console.log('This is the error in the api/signupTable.ts file:', error);
+      console.error("This is the error in the api/signupTable.ts file:", error);
       return res.status(500).json({ message: "Internal server error", error });
     }
   } else {
-    console.log('Called a method other than post for supa operations');
+    console.log("Called a method other than POST for supa operations");
     return res.status(405).json({ message: "Method Not Allowed" });
   }
 }
