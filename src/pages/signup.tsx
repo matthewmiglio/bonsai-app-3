05882a6e0e367eb42bf1086cd7e68e839009330e
Matt Main_PC <matmigg0804@gmail.com>
2025-02-10 11:40:21 -0500

    removing unused var

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import { useRouter } from "next/navigation";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { Button } from "@/components/ui/button";
