c7b10746179718873fdcc801843a0e5380817b56
Matt Main_PC <matmigg0804@gmail.com>
2025-03-18 10:48:14 -0400

    linting

--- src/pages/api/addToEmailsTable.ts
+++ src/pages/api/addToEmailsTable.ts
@@ -1,11 +1,16 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPA_URL || "no supa table url in env";
-const supabaseKey = process.env.NEXT_PUBLIC_SUPA_SECRET_KEY || "no supa key in env";
+const supabaseUrl =
+  process.env.NEXT_PUBLIC_SUPA_URL || "no supa table url in env";
+const supabaseKey =
+  process.env.NEXT_PUBLIC_SUPA_SECRET_KEY || "no supa key in env";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method === "POST") {
     const { email } = req.body;
 
@@ -16,15 +21,19 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     try {
       console.log("Received request to add email:", email);
 
-      const { data, error } = await supabase.from("EMAILs").insert([
-        { email, created_at: new Date().toISOString() },
-      ]);
+      const { data, error } = await supabase
+        .from("EMAILs")
+        .insert([{ email, created_at: new Date().toISOString() }]);
 
       if (error) {
-        return res.status(400).json({ message: "Error inserting email", error });
+        return res
+          .status(400)
+          .json({ message: "Error inserting email", error });
       }
 
-      return res.status(200).json({ message: "Email successfully added", data });
+      return res
+        .status(200)
+        .json({ message: "Email successfully added", data });
     } catch (error) {
       console.error("Error in API:", error);
       return res.status(500).json({ message: "Internal server error", error });
