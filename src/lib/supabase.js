71662fe043662451401d27f7b2e8cde7883681b5
Matthew <matmigg0804@gmail.com>
2025-03-18 08:08:28 -0400

    verbose off

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const verbose = true;
+const verbose = false;
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPA_URL || "no supa url in env";
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPA_PUBLIC_KEY || "no supa anon key in env";
