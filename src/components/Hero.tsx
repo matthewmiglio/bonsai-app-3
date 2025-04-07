f0f60b7289d562c3a040dd54f49cd118703a3c42
Matt Main_PC <matmigg0804@gmail.com>
2025-04-07 11:35:15 -0400

    new font

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -34,9 +34,17 @@ export default function Hero() {
         />
       ))}
       <div className="relative z-10 text-center text-white">
-        <h1 className="text-5xl font-bold mb-4">Cultivating Tranquility</h1>
-        <p className="text-xl">
-        Discover the art and beauty of bonsai
+        <h1
+          className="text-6xl mb-4 font-normal"
+          style={{ fontFamily: "'Lobster', cursive" }}
+        >
+          Cultivating Tranquility
+        </h1>
+
+        <p
+          className="text-lg font-light"
+        >
+          Discover the art and beauty of bonsai
         </p>
       </div>
     </section>
