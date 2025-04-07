47e9e8641dd06675f54e98b4cc87a626c6f50f99
Matt Main_PC <matmigg0804@gmail.com>
2025-04-07 11:35:48 -0400

    include google fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import "@/styles/globals.css";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
+      <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet" />
       <RootLayout>
         <Component {...pageProps} />
       </RootLayout>
