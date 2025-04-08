8d3510699ac1bd5560addf52e128d21a14c78f80
Matt Main_PC <matmigg0804@gmail.com>
2025-04-08 10:14:34 -0400

    vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,11 @@ import type { AppProps } from "next/app";
 import RootLayout from "@/layouts/RootLayout";
 import { SessionProvider } from "next-auth/react";
 import "@/styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
-      {/* <link
-        href="https://fonts.googleapis.com/css2?family=Lobster&display=swap"
-        rel="stylesheet"
-      /> */}
       <link
         href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap"
         rel="stylesheet"
@@ -17,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <RootLayout>
         <Component {...pageProps} />
       </RootLayout>
+      <Analytics /> {/*Standard to put analytics inside app as close to root as possible but within providers*/}
     </SessionProvider>
   );
 }
